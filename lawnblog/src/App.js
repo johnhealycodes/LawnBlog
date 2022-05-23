0c060846bb8315e7e90ea91931127f631c15f2d5
johnhealycodes <93292715+johnhealycodes@users.noreply.github.com>
2022-05-22 23:15:16 -0500

    Created Mobile Friendly NavBar

--- lawnblog/src/App.js
+++ lawnblog/src/App.js
@@ -1,40 +1,12 @@
-// import logo from "./logo.svg";
+import React from "react";
+import Navbar from "./components/NavBar/NavBar";
 import "./App.css";
 
 function App() {
   return (
-    <div class="navBar">
-      <a href="#">Plot</a>
-      <ul>
-        <a href="#">Post</a>
-      </ul>
-      <ul>
-        <a href="#">Trending</a>
-      </ul>
-      <ul>
-        <a href="#">Notifications</a>
-      </ul>
-      <ul>
-        <a href="#">Sign In</a>
-      </ul>
+    <div class="App">
+      <Navbar />
     </div>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
