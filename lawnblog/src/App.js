51fac8759dfd54f856bd673a59a767ee0404b0d2
johnhealycodes <93292715+johnhealycodes@users.noreply.github.com>
2022-05-22 21:17:09 -0500

    started react app

--- lawnblog/src/App.js
+++ lawnblog/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="navBar">
+      <a href="#">Plot</a>
+      <ul>
+        <a href="#">Post</a>
+      </ul>
+      <ul>
+        <a href="#">Trending</a>
+      </ul>
+      <ul>
+        <a href="#">Notifications</a>
+      </ul>
+      <ul>
+        <a href="#">Sign In</a>
+      </ul>
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
